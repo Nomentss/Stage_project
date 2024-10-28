ec0d8c6035a56e13d0903d0805a7eb88167bd9fb
Nomentss5 <119040360+Nomentss@users.noreply.github.com>
2024-10-28 08:52:29 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Paginator::useBootstrap();
         Paginator::useBootstrapFive();
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        } 
     }
 }
